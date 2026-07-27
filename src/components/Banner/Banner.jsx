71081f56a347d2531f1ae0041ef65fbd3111a43b
asm-saim <saim777m@gmail.com>
2026-07-27 16:16:07 +0600

    banner section completed

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,53 +1,62 @@
-import React from 'react';
-import bannerImg from "../../assets/hero.png";
+import React from "react";
+import bannerImg from "../../assets/banner.png";
 import { BsGooglePlay } from "react-icons/bs";
 import { RiAppStoreFill } from "react-icons/ri";
+import { FaGithub, FaLinkedin } from "react-icons/fa";
 
 const Banner = () => {
-    return (
-        <div className="max-w-7xl mx-auto px-4 mt-10 flex flex-col items-center text-center">
+  return (
+    <div className="max-w-7xl mx-auto px-4 flex flex-col lg:flex-row items-center text-center">
+      <div className="flex-1">
+        <h1 className="text-4xl md:text-5xl lg:text-6xl font-bold leading-tight">
+          Hi, I'm Saim.
+        </h1>
+        <h2 className="text-3xl md:text-4xl lg:text-4xl font-bold leading-tight">
+          <span className="text-blue-400">Frontend Developer</span>
+        </h2>
 
-            <h1 className="text-4xl md:text-5xl lg:text-6xl font-bold leading-tight">
-                We Build <br />
-                <span className="text-blue-400">Productive</span> Apps
-            </h1>
+        <p className="mt-6 text-gray-300 italic max-w-3xl text-sm md:text-base leading-7">
+          I build fast, responsive, and user-friendly web applications with
+          clean, maintainable code. Currently focused on turning ideas into
+          seamless digital experiences. I enjoy solving problems through code
+          and continuously improving how I build for the web.
+        </p>
 
-            <p className="mt-6 text-gray-300 italic max-w-3xl text-sm md:text-base leading-7">
-                At Simplico, we craft innovative apps designed to make everyday life
-                simpler, smarter, and more exciting. Our goal is to turn your ideas
-                into digital experiences that truly make an impact.
-            </p>
+        <div className="mt-8 flex justify-center flex-col sm:flex-row gap-4">
+          <a
+            href="https://github.com/asm-saim"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="btn btn-sm md:btn-md border-none rounded-lg bg-gradient-to-br from-[#0B2F5B] to-[#3B82F6]"
+          >
+            <FaGithub className="text-lg" />
+            GitHub
+          </a>
 
-            <div className="mt-8 flex flex-col sm:flex-row gap-4">
-                <a
-                    href="https://play.google.com"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className="btn btn-sm md:btn-md border-none rounded-lg bg-gradient-to-br from-[#0B2F5B] to-[#3B82F6]"
-                >
-                    <BsGooglePlay className="text-lg" />
-                    Google Play
-                </a>
-
-                <a
-                    href="https://www.apple.com/app-store/"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className="btn btn-sm md:btn-md border-none rounded-lg bg-gradient-to-br from-[#0B2F5B] to-[#3B82F6]"
-                >
-                    <RiAppStoreFill className="text-lg" />
-                    App Store
-                </a>
-            </div>
-
-            <img
-                src={bannerImg}
-                alt="Banner"
-                className=" mt-10 w-full max-w-xs sm:max-w-sm md:max-w-md lg:max-w-none
-                lg:h-[55vh] object-contain"
-            />
+          <a
+            href="https://www.linkedin.com/in/asmsaim/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="btn btn-sm md:btn-md border-none rounded-lg bg-gradient-to-br from-[#0B2F5B] to-[#3B82F6]"
+          >
+            <FaLinkedin className="text-lg" />
+            LinkedIn
+          </a>
         </div>
-    );
+      </div>
+      <div className="flex-1 relative flex justify-center items-center">
+        {/* Blurred gradient */}
+        <div className="absolute w-[500px] h-[500px] sm:w-96 sm:h-96 rounded-full bg-gradient-to-br from-[#0B2F5B] via-[#3B82F6] to-cyan-400 blur-[120px] opacity-40"></div>
+
+        {/* Image */}
+        <img
+          src={bannerImg}
+          alt="Banner"
+          className="relative z-10 mt-10 w-full max-w-xs sm:max-w-sm md:max-w-md lg:max-w-none lg:h-[81vh] object-contain"
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Banner;
